9685c34439a9b2f53f7e685cd22439ce5fa44d09
abylander <andrewbylander@hotmail.com>
2015-12-15 22:03:51 -0600

    ASP.NET
    Part D

---
+++ MVCWebApp/MVCWebApp/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using MVCWebApp.Context;
+using MVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductContext db = new ProductContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: Product/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
